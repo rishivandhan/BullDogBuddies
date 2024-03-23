72747dd5e5dddaa726c9a2693c24ff9542ae6449
RishiMusuvathi <rishimusuvathi03@gmail.com>
2024-03-23 16:06:22 -0400

    vs code changed formatting

--- bulldog-buddies/src/components/navBar.js
+++ bulldog-buddies/src/components/navBar.js
@@ -1,35 +1,39 @@
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
-import { useState } from 'react';
-import './navBar.css';
+import { useState } from "react";
+import "./navBar.css";
 import Signup from "./Signup";
 
 function Navbar() {
-    const navRef = useRef();
-    const showNavbar = () => {
-        navRef.current.classList.toggle("responsive_nav");
-    }
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-    const [signupPopup, setButtonPopup] = useState(false);
-    return (
-        <header>
-            <h3><a href="/">Bulldog Buddies</a></h3>
-            <nav ref={navRef}>
-                <a href="/">Log in</a>
-                <button className="signup-btn"onClick={() => setButtonPopup(true)}>Sign up</button>
-                <Signup trigger={signupPopup} setTrigger={setButtonPopup}>
-                    <h3>Sign up</h3>
-                    <p>sign up form</p>
-                </Signup>
-                <button className="nav-btn nav-close" onClick={showNavbar}>
-                    <FaTimes />
-                </button>
-            </nav>
-            <button className="nav-btn nav-close" onClick={showNavbar}>
-                <FaBars />
-            </button>
-        </header>
-    );
-  }
+  const [signupPopup, setButtonPopup] = useState(false);
+  return (
+    <header>
+      <h3>
+        <a href="/">Bulldog Buddies</a>
+      </h3>
+      <nav ref={navRef}>
+        <a href="/">Log in</a>
+        <button className="signup-btn" onClick={() => setButtonPopup(true)}>
+          Sign up
+        </button>
+        <Signup trigger={signupPopup} setTrigger={setButtonPopup}>
+          <h3>Sign up</h3>
+          <p>sign up form</p>
+        </Signup>
+        <button className="nav-btn nav-close" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn nav-close" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
+}
 
-  export default Navbar;
+export default Navbar;
