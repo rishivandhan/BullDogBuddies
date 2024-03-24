b4d6a954537c0e367ad8da4a53e76c66464ce222
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-03-23 22:01:22 -0400

    Update Signup.js

--- bulldog-buddies/src/components/Signup.js
+++ bulldog-buddies/src/components/Signup.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { FaTimes, FaBars } from 'react';
 import './Signup.css'
 
 function Signup(props) {
     return (props.trigger) ? (
         <div className="signup">
             <div className="signup-inner">
+                <button className="close-btn" onClick={() => props.setTrigger(false)}>
+                <FaTimes />
+                </button>
+                <button className="nav-btn nav-close">
+                <FaBars />
+                </button>
                 { props.children }
             </div>
         </div>
