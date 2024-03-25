f9294503f8f83f472ec976d172fb555c204d2c02
Rishivandhan Musuvathi <rishimusuvathi03@gmail.com>
2024-03-25 11:55:31 -0400

    finished making the home page, must format the css file to make it look accurate

--- bulldog-buddies/src/App.js
+++ bulldog-buddies/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import './App.css'
+import React from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+
+      <Home />
     </React.Fragment>
   );
 }
---
+++ bulldog-buddies/src/components/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Navbar.css";
+import "./Home.css";
+
+import HighFive from "../Assets/High five-amico 1.jpg";
+import partyPicture from "../Assets/party-pana 1.jpg";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="page-heading">
+        <h1>Whats your next event?</h1>
+        <h3 className="Page-subheading">
+          The easiest way for finding your next (scroll through word list)
+        </h3>
+      </div>
+
+      <div className="welcome-picture">
+        <img src={partyPicture} alt="" />
+      </div>
+
+      <div className="highFive-Picture">
+        <img src={HighFive} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
