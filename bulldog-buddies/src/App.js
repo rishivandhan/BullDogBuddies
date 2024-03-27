8947278eeeab00e6880d7efc8c594a792b919107
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-03-27 14:43:14 -0400

    toni-changes

--- bulldog-buddies/src/App.js
+++ bulldog-buddies/src/App.js
@@ -1,11 +1,25 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
+=======
+import React from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./App.css";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
+<<<<<<< Updated upstream
+=======
+
+      <Home />
+      <Footer />
+>>>>>>> Stashed changes
     </React.Fragment>
   );
 }
---
+++ bulldog-buddies/src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Footer.css';
+import { FaGithub } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div classNAme="sb__footer section__padding">
+                <div className="sb__footer-links">
+                    <div className="sb__footer-links-div">
+                        <p><img src={ FaGithub } alt="github" /></p>
+                    </div>
+                    <div className="sb__footer-links-div">
+                        <p>Rishi</p>
+                        <p>Toni</p>
+                        <p>Arjun</p>
+                        <p>Sanjay</p>
+                        <p>Luke</p>
+                    </div>
+                </div>
+            </div>
+            <div className="sb__footer-below">
+                <div className="sb__footer-copyright">
+                    <p>
+                        @{new Date().getFullYear()} Bulldog Buddies. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
