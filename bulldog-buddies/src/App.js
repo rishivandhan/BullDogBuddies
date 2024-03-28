1745cb51fdee20d7d741cc82b8548f3d560bf39b
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-03-27 20:23:36 -0400

    Revert "toni-changes"
    This reverts commit 3f520deb97b14946b0ff286c6a261c0e88251fea.

--- bulldog-buddies/src/App.js
+++ bulldog-buddies/src/App.js
@@ -1,28 +1,22 @@
+<<<<<<< Updated upstream
+import React from 'react'
+import Navbar from './components/Navbar'
+import './App.css'
+=======
 import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./App.css";
+>>>>>>> Stashed changes
 
 function App() {
   return (
-    <React.Fragment>
-
+    <div className="App">
       <Navbar />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      
-      
-
       <Home />
       <Footer />
-    </React.Fragment>
+    </div>
   );
 }
 
