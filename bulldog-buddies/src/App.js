ecaf6359a9888be9249742ef8ade3aefe9f5a9db
lukeup12 <lukeup12@gmail.com>
2024-04-02 10:32:21 -0400

    creation of event pages + sidebar

--- bulldog-buddies/src/App.js
+++ bulldog-buddies/src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React from "react";  
 import Home from "./components/Home";
+import MyEvents from "./components/MyEvents";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import CreateEvents from "./components/CreateEvents";
+import ViewEvents from "./components/ViewEvents";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
+    <React.Fragment>
       <Navbar />
-      <Home />
-      <Footer />
+    </React.Fragment>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/MyEvents" element ={<MyEvents />} />
+      <Route path="/CreateEvents" element ={<CreateEvents />} />
+      <Route path="/ViewEvents" element ={<ViewEvents />} />
+    </Routes>
     </div>
+    
   );
 }
 
