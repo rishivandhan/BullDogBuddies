49d3a5ed86c2cbccbc9c2975cdb7c8fa4fb22410
Rishivandhan Musuvathi <rishimusuvathi03@gmail.com>
2024-04-03 16:10:29 -0400

    Revert "Merge branch 'luke-changes-2'"
    This reverts commit ceabc9f05618530f49588db5cecf02fec3bda4e5, reversing
    changes made to 363f12e850db4111af6abca71e88b5414c373599.

--- bulldog-buddies/src/App.js
+++ bulldog-buddies/src/App.js
@@ -1,27 +1,16 @@
-import React from "react";  
+import React from "react";
 import Home from "./components/Home";
-import MyEvents from "./components/MyEvents";
 import Navbar from "./components/Navbar";
-import CreateEvents from "./components/CreateEvents";
-import ViewEvents from "./components/ViewEvents";
+import Footer from "./components/Footer";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-
 
 function App() {
   return (
-    <div>
-    <React.Fragment>
+    <div className="App">
       <Navbar />
-    </React.Fragment>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/MyEvents" element ={<MyEvents />} />
-      <Route path="/CreateEvents" element ={<CreateEvents />} />
-      <Route path="/ViewEvents" element ={<ViewEvents />} />
-    </Routes>
+      <Home />
+      <Footer />
     </div>
-    
   );
 }
 
