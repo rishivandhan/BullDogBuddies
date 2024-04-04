b754c15420a2726774ddbcd3055e3d3a09c46bc2
Rishivandhan Musuvathi <rishimusuvathi03@gmail.com>
2024-04-03 22:58:06 -0400

    can create Events and store in database

--- bulldog-buddies/src/components/CreateEvents.js
+++ bulldog-buddies/src/components/CreateEvents.js
@@ -1,69 +1,100 @@
-import React from 'react';
-import "./CreateEvents.css"
-import Sidebar from './sidebar';
-import { useEventOperations } from '../FirebaseEventOperations';
-import { useNavigate } from 'react-router-dom';
-
-
+import React from "react";
+import "./CreateEvents.css";
+import Sidebar from "./sidebar";
+import { useEventOperations } from "../FirebaseEventOperations";
 
 function CreateEvents() {
+  const { createEvent } = useEventOperations();
 
-    //const { bring in functions from firebaseeventOperations} = useEventOperations(); 
+  function handleEventCreate(e) {
+    console.log("submit was clicked");
+    e.preventDefault();
 
-  
-    function handleEventCreate(e) {
-        
-      console("submit was clicked");
-      e.preventDefault();
+    //constant stores values entered into textbox
+    const title = document.getElementById("Title").value;
+    const ExpirationTime = document.getElementById("ExpirationTime").value;
+    const description = document.getElementById("description").value;
+    const location = document.getElementById("location").value;
+    const eventPeopleRegistered =
+      document.getElementById("PeopleRegistered").value;
+    const time = document.getElementById("Time").value;
+    const Limit = document.getElementById("Limit").value;
 
-
-      //constant stores values entered into textbox
-      const title = document.getElementById("Title");
-      const ExpirationTime = document.getElementById("ExpirationTime");
-      const description = document.getElementById("description");
-      const location = document.getElementById("location");
-      const eventPeopleRegistered = document.getElementById("eventPeopleRegistered");
-      const time = document.getElementById("Time");
-      const Limit = document.getElementById("Limit");
-      
-
-      //check to see if fields were entered
-      if(!title || !ExpirationTime || !description || !location || !eventPeopleRegistered || !time || !Limit){
-        alert("Please fill in all the required fields");
-        return;
-      }
-
+    //check to see if fields were entered or else create event;
+    if (
+      !title ||
+      !ExpirationTime ||
+      !description ||
+      !location ||
+      !eventPeopleRegistered ||
+      !time ||
+      !Limit
+    ) {
+      alert("Please fill in all the required fields");
+      return;
+    } else {
+      createEvent(
+        title,
+        ExpirationTime,
+        description,
+        location,
+        eventPeopleRegistered,
+        time
+      );
+      alert("event Created Successfully");
     }
-    return(
-        <div className='CreateEvents'>
-            <div className='sidebar'>
-            <React.Fragment>
-                <Sidebar />
-            </React.Fragment>
-            </div>
+  }
+  return (
+    <div className="CreateEvents">
+      <div className="sidebar">
+        <React.Fragment>
+          <Sidebar />
+        </React.Fragment>
+      </div>
 
-        <div className='form'>
-        <form method="post" onSubmit={handleEventCreate}>
+      <div className="form">
+        <form onSubmit={handleEventCreate}>
+          <label>
+            Title: <input name="eventTitle" defaultValue="" id="Title" /> <br />
+            Expiration Time:{" "}
+            <input
+              name="eventExpirationTime"
+              defaultValue=""
+              id="ExpirationTime"
+            />{" "}
+            <br />
+            Description:{" "}
+            <input name="Description" defaultValue="" id="description" />
+            <br />
+            Location:{" "}
+            <input name="eventLocation" defaultValue="" id="location" />
+            <br />
+            Number of People to Resgister:{" "}
+            <input
+              name="eventPeopleRegistered"
+              defaultValue=""
+              id="PeopleRegistered"
+            />
+            <br />
+            Time: <input
+              name="eventTitle"
+              defaultValue=""
+              id="Time"
+            /> <br />{" "}
+            {/*come back later. not a text input. need a checkbox for am and pm*/}
+            Number of People Limit:{" "}
+            <input name="PeopleLimit" defaultValue="" id="Limit" />
+            <br />
+          </label>
 
-
-      <label>
-        Title: <input name="eventTitle" defaultValue="" id = "Title"/> <br />
-        Expiration Time: <input name="eventExpirationTime" defaultValue="" id = "ExpirationTime"/> <br />
-        Description: <input name="Descriptio" defaultValue="" id = "description"/><br />
-        Location: <input name="eventLocation" defaultValue="" id = "location"/><br />
-        Number of People to Resgister: <input name="eventPeopleRegistered" defaultValue="" id = "PeopleRegistered"/><br />
-        Time: <input name="eventTitle" defaultValue="" id = "Time"/>  <br /> {/*come back later. not a text input. need a checkbox for am and pm*/  }  
-        Number of People Limit: <input name="PeopleLimit" defaultValue="" id ="Limit" /><br />
-
-      </label>
-
-
-      <button type="reset">Reset form</button>
-      <button type="submit" id = "submit-event-button">Submit form</button>
-      </form>
-        </div>
+          <button type="reset">Reset form</button>
+          <button type="submit" id="submit-event-button">
+            Submit form
+          </button>
+        </form>
+      </div>
     </div>
-    );
+  );
 }
 
 export default CreateEvents;
