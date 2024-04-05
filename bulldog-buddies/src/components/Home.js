5843506186f4a4f9bccd6a2e90e1d2d1757d1a73
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-04-04 21:28:43 -0400

    Revert "Merge branch 'luke-changes-2'"
    This reverts commit c8d5af194d919cb489a556fa5d8d36d481de7cc7, reversing
    changes made to 672dfa1f8b66a0d135e074fffe271e7039c82bf9.

--- bulldog-buddies/src/components/Home.js
+++ bulldog-buddies/src/components/Home.js
@@ -3,14 +3,11 @@ import "./Home.css";
 import { ReactTyped } from 'react-typed';
 import HighFive from "../Assets/High five-amico 1.jpg";
 import partyPicture from "../Assets/party-pana 1.jpg";
-import Navbar from './Navbar';
-import Footer from './Footer';
 
 function Home() {
   return (
     <div class="home-container">
-      <Navbar />
-      <div class="left-side">
+      <div class="page-heading">
         <h1>Whats your next event?</h1>
         <h3 class="Page-subheading">
           The easiest way for finding your next
@@ -38,7 +35,6 @@ function Home() {
           Connect with students and people in your city
         </h3>
       </div>
-      <Footer />
     </div>
   );
 }
