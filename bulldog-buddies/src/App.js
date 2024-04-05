a57ec943ee5a1ba6551865b9fd9a9c40639cecf0
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-04-04 22:38:45 -0400

    revert

--- bulldog-buddies/src/App.js
+++ bulldog-buddies/src/App.js
@@ -1,29 +1,21 @@
 import React from "react";
 import Home from "./components/Home";
 import MyEvents from "./components/MyEvents";
-import Navbar from "./components/Navbar";
 import CreateEvents from "./components/CreateEvents";
 import ViewEvents from "./components/ViewEvents";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
 
 function App() {
   return (
 
     <div>
-    <React.Fragment>
-      <Navbar />
-    </React.Fragment>
     <Routes>
       <Route path="*" element={<Home/>} />
       <Route path="/MyEvents" element ={<MyEvents />} />
       <Route path="/CreateEvents" element ={<CreateEvents />} />
       <Route path="/ViewEvents" element ={<ViewEvents />} />
     </Routes>
-    <React.Fragment>
-    <Footer />
-    </React.Fragment>
     </div>
   );
 }
--- bulldog-buddies/src/components/Home.js
+++ bulldog-buddies/src/components/Home.js
@@ -3,23 +3,20 @@ import "./Home.css";
 import { ReactTyped } from 'react-typed';
 import HighFive from "../Assets/High five-amico 1.jpg";
 import partyPicture from "../Assets/party-pana 1.jpg";
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 function Home() {
   return (
     <div class="home-container">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
       <Navbar />
->>>>>>> parent of 730f396 (toni-updates)
-=======
->>>>>>> parent of 08841be (tonis-updates)
-      <div class="page-heading">
-        <h1>Whats your next event?</h1>
-        <h3 class="Page-subheading">
-          The easiest way for finding your next
-        </h3>
-        <h3 className="body-highlight">
+      <div class="left-side">
+        <div class="page-heading">
+          <h1>Whats your next event?</h1>
+          <h3 class="Page-subheading">
+            The easiest way for finding your next
+          </h3>
+          <h3 className="body-highlight">
           <ReactTyped
             strings={[
               "tennis partner",
@@ -31,7 +28,8 @@ function Home() {
             backSpeed={100}
             loop
           />
-        </h3>
+          </h3>
+        </ div>
         <div class="welcome-Picture">
           <img src={partyPicture} alt="welcome-picture" />
         </div>
@@ -42,6 +40,7 @@ function Home() {
           Connect with students and people in your city
         </h3>
       </div>
+      <Footer />
     </div>
   );
 }
