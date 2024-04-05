7bbb6cfb19730d22b5c3f4a65df08a862f6fa57d
Antonio Mendoza <96957120+amendoza09@users.noreply.github.com>
2024-04-04 21:26:09 -0400

    Revert "Merge branch 'luke-changes-2'"
    This reverts commit c8d5af194d919cb489a556fa5d8d36d481de7cc7, reversing
    changes made to 672dfa1f8b66a0d135e074fffe271e7039c82bf9.

--- bulldog-buddies/src/App.js
+++ bulldog-buddies/src/App.js
@@ -1,25 +1,16 @@
-import React from "react";  
+import React from "react";
 import Home from "./components/Home";
-import MyEvents from "./components/MyEvents";
 import Navbar from "./components/Navbar";
-import CreateEvents from "./components/CreateEvents";
-import ViewEvents from "./components/ViewEvents";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-
+import "./App.css";
 
 function App() {
   return (
-    <div>
-    <Routes>
-      <Route path="*" element={<Home/>} />
-      <Route path="/MyEvents" element ={<MyEvents />} />
-      <Route path="/CreateEvents" element ={<CreateEvents />} />
-      <Route path="/ViewEvents" element ={<ViewEvents />} />
-    </Routes>
+    <div className="App">
+      <Navbar />
+      <Home />
+      <Footer />
     </div>
-    
   );
 }
 
