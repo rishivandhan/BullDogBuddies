2b9f565d02b386313ed1a27fe1f8fddc29739f3b
ArjunPuritipati <arjun.puritipati@gmail.com>
2024-04-19 11:29:30 -0400

    Able to view RSVP events and Created events

--- bulldog-buddies/src/components/MyEvents.js
+++ bulldog-buddies/src/components/MyEvents.js
@@ -2,26 +2,92 @@ import React from "react";
 import Link from "react-router-dom";
 import "./sidebar.css";
 import "./MyEvents.css";
+import { Card } from "react-bootstrap";
+import ItemsSlider from "./ItemsSlider";
+import { database } from '../Firebase';
+import { push, ref, onValue, set, update, remove } from 'firebase/database';
+import { useEventOperations } from "../FirebaseEventOperations";
+import { useState, useEffect, useRef } from "react";
 
 import Sidebar from "./sidebar";
 
-function MyEvents() {
-  return (
-    <div className="my-events-container">
-      <div className="sidebar">
-        <Sidebar />
-      </div>
-      <div className="view-display">
-        <div className="CEvent-card">
-          <h2 className="title"> </h2>
-          <p className="description"></p>
-          <p className="time"></p>
-          <p className="location"></p>
-          <p className="participants">Participants:</p>
-          <button className="rsvp-button">Delete Created Event</button>
+
+function MyEvents(){
+    const [createdEvents, setCreatedEvents] = useState([]);
+  const currentUserId = localStorage.getItem("currentUserId"); // or however you obtain the current user's ID
+
+  useEffect(() => {
+    console.log("Fetching created events for user ID:", currentUserId);
+    const userCreatedEventsRef = ref(database,'users/' + currentUserId + '/UserCreatedEvents');
+    
+    onValue(userCreatedEventsRef, (snapshot) => {
+      if (snapshot.exists()) {
+        console.log("Found created events for user:", currentUserId, snapshot.val());
+        const createdEventIds = Object.keys(snapshot.val());
+
+        const eventPromises = createdEventIds.map((eventId) => {
+          return new Promise((resolve) => {
+            const eventRef = ref(database, 'events/' + eventId);
+            onValue(eventRef, (eventSnapshot) => {
+              if (eventSnapshot.exists()) {
+                console.log(`Event data fetched for event ID: ${eventId}`, eventSnapshot.val());
+                resolve({ id: eventId, ...eventSnapshot.val() });
+              } else {
+                console.log(`No data found for event ID: ${eventId}`);
+                resolve(null);
+              }
+            }, {
+              onlyOnce: true
+            });
+          });
+        });
+
+        Promise.all(eventPromises).then((events) => {
+          const validEvents = events.filter(event => event !== null);
+          console.log("All created event data fetched:", validEvents);
+          setCreatedEvents(validEvents);
+        });
+      } else {
+        console.log("No created events found for user:", currentUserId);
+        setCreatedEvents([]);
+      }
+    }, {
+      onlyOnce: true
+    });
+    
+    }, [currentUserId]);
+
+    const handleRSVP = () => {
+
+    };
+
+    return (
+        <div className="mycreatedevents-container">
+            <div className="sidebar">
+                <Sidebar />
+            </div>
+            <div className="view-display">
+                {createdEvents.length > 0 ? createdEvents.map((event) => (
+                <div key={event.id} className="created-event-card">
+                    <h2 className="title">{event.title}</h2>
+                    <p className="description">{event.description}</p>
+                    <p className="time">Time: {event.time}</p>
+                    <p className="location">Location: {event.location}</p>
+                    <p className="participants">
+                    Participants: {event.numberOfPeopleRegistered}/{event.numberOfPeopleLimit}
+                    </p>
+                    <button onClick={() => handleRSVP(event.id)} className="rsvp-button">
+                        Edit
+                    </button>
+                    <button onClick={() => handleRSVP(event.id)} className="rsvp-button">
+                        Delete
+                    </button>
+                </div>
+                )) : (
+                <p>No events created by you to show.</p>
+                )}
+            </div>
         </div>
-      </div>
-    </div>
   );
 }
 
--- bulldog-buddies/src/components/Viewrsvp.js
+++ bulldog-buddies/src/components/Viewrsvp.js
@@ -2,31 +2,90 @@ import React from 'react';
 import Link from 'react-router-dom';
 import "./sidebar.css";
 import "./Viewrsvp.css";
+import { Card } from "react-bootstrap";
+import ItemsSlider from "./ItemsSlider";
+import { database } from '../Firebase';
+import { push, ref, onValue, set, update, remove } from 'firebase/database';
+import { useEventOperations } from "../FirebaseEventOperations";
+import { useState, useEffect, useRef } from "react";
 
 import Sidebar from "./sidebar";
 
 
 function Viewrsvp(){
-    return(
-         <div className="myrsvp-container">
+
+    const [userRsvpEvents, setUserRsvpEvents] = useState([]);
+    const currentUserId = localStorage.getItem("currentUserId");
+    const { handleRSVP, handleUndoRSVP } = useEventOperations();
+
+    useEffect(() => {
+        console.log("Fetching RSVP events for user ID:", currentUserId);
+        const userRSVPList = 'users/' + currentUserId + '/UserRSVPEvents'
+        const userRsvpRef = ref(database, userRSVPList);
+        
+        onValue(userRsvpRef, (snapshot) => {
+            if (snapshot.exists()) {
+                console.log("Found RSVPs for user:", currentUserId, snapshot.val());
+                const rsvpEventIds = Object.keys(snapshot.val());
+
+                const eventPromises = rsvpEventIds.map((eventId) => {
+                    return new Promise((resolve) => {
+                        const eventRefString = 'events/' + eventId;
+                        const eventRef = ref(database, eventRefString);
+                        onValue(eventRef, (eventSnapshot) => {
+                            if (eventSnapshot.exists()) {
+                                console.log('Event data fetched for event ID: ${eventId}', eventSnapshot.val());
+                                resolve({ id: eventId, ...eventSnapshot.val() });
+                            } else {
+                                console.log('No data found for event ID: ${eventId}');
+                                resolve(null);
+                            }
+                        }, {
+                            onlyOnce: true
+                        });
+                    });
+                });
+
+                Promise.all(eventPromises).then((events) => {
+                    const validEvents = events.filter(event => event !== null);
+                    console.log("All RSVP event data fetched:", validEvents);
+                    setUserRsvpEvents(validEvents);
+                });
+            } else {
+                console.log("No RSVPs found for user:", currentUserId);
+                setUserRsvpEvents([]);
+            }
+        }, {
+            onlyOnce: true
+        });
+
+        
+    }, [currentUserId]);
+
+    return (
+        <div className="myrsvp-container">
             <div className="sidebar">
-                    <Sidebar />
+                <Sidebar />
             </div>
             <div className="view-display">
-            <div className="rsvp-card">
-                <h2 className="title"> </h2>
-                <p className="description"></p>
-                <p className="time"></p>
-                <p className="location"></p>
-                <p className="participants">
-                    Participants:
-                </p>
-                <button className="rsvp-button">
-                    Cancel RSVP
-                </button>
+                {userRsvpEvents.length > 0 ? userRsvpEvents.map((event) => (
+                    <div key={event.id} className="rsvp-card">
+                        <h2 className="title">{event.title}</h2>
+                        <p className="description">{event.description}</p>
+                        <p className="time">Time: {event.time}</p>
+                        <p className="location">Location: {event.location}</p>
+                        <p className="participants">
+                            Participants: {event.numberOfPeopleRegistered}/{event.numberOfPeopleLimit}
+                        </p>
+                        <button onClick={() => handleRSVP(event.id)} className="rsvp-button">
+                            Undo RSVP
+                        </button>
+                    </div>
+                )) : (
+                    <p>No RSVP events to show</p>
+                )}
             </div>
-      </div>
-    </div>
+        </div>
     );
 }
 
